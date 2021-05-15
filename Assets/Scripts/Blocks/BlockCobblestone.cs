9432c8245c7f79c0ad60e9bd46e6c94d083a1519
Fehniix <29045904+Fehniix@users.noreply.github.com>
2021-05-15 16:50:10 +0200

    Added began breaking delegate

--- Assets/Scripts/Blocks/BlockCobblestone.cs
+++ Assets/Scripts/Blocks/BlockCobblestone.cs
@@ -6,17 +6,24 @@ public class BlockCobblestone : MonoBehaviour
 {
     void Start()
     {
-		Block block 			= this.GetComponent<Block>();
-		block.blockName 		= "cobblestone";
-		block.hasSidedTextures	= false;
-		block.hardness		 	= 2 * 20;
-		block.breakDelegate 	= this.Break;
+		Block block 				= this.GetComponent<Block>();
+		block.blockName 			= "cobblestone";
+		block.hasSidedTextures		= false;
+		block.hardness		 		= 2 * 20;
+		block.breakDelegate 		= this.Break;
+		block.beganBreakingDelegate = this.BeganBreaking;
 
 		this.GetComponent<TextureLoader>().LoadTexture();
     }
 
+	void BeganBreaking()
+	{
+		//TODO Access currently active item to modify the block's hardness.
+		Debug.Log("Began breaking");
+	}
+
 	void Break()
 	{
-		Debug.Log("BLOCK BROKEN!");
+		Destroy(this.gameObject);
 	}
 }
