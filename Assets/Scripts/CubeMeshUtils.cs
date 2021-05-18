329041c6eb26ff6bf6488f7cd61614cb30ee8871
Fehniix <29045904+Fehniix@users.noreply.github.com>
2021-05-18 17:00:34 +0200

    First commit

---
+++ Assets/Scripts/CubeMeshUtils.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Extensions;
+
+/// <summary>
+/// Contains vertices information for each cube face.
+/// </summary>
+public struct CubeMeshFaces
+{
+	public static readonly Vector3[] front = {
+		(0,0,0).ToVector3(),
+		(0,1,0).ToVector3(),
+		(1,1,0).ToVector3(),
+		(1,0,0).ToVector3()
+	};
+
+	public static readonly Vector3[] back = {
+		(1,0,1).ToVector3(),
+		(1,1,1).ToVector3(),
+		(0,1,1).ToVector3(),
+		(0,0,1).ToVector3()
+	};
+
+	public static readonly Vector3[] top = {
+		(0,1,0).ToVector3(),
+		(0,1,1).ToVector3(),
+		(1,1,1).ToVector3(),
+		(1,1,0).ToVector3()
+	};
+
+	public static readonly Vector3[] bottom = {
+		(1,0,0).ToVector3(),
+		(1,0,1).ToVector3(),
+		(0,0,1).ToVector3(),
+		(0,0,0).ToVector3()
+	};
+
+	public static readonly Vector3[] west = {
+		(0,0,1).ToVector3(),
+		(0,1,1).ToVector3(),
+		(0,1,0).ToVector3(),
+		(0,0,0).ToVector3()
+	};
+
+	public static readonly Vector3[] east = {
+		(1,0,0).ToVector3(),
+		(1,1,0).ToVector3(),
+		(1,1,1).ToVector3(),
+		(1,0,1).ToVector3()
+	};
+}
