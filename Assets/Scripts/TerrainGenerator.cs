a889aadd889021283894b55f982ef96c9e30479e
Fehniix <29045904+Fehniix@users.noreply.github.com>
2021-05-18 10:08:42 +0200

    Fixed small typo.

--- Assets/Scripts/TerrainGenerator.cs
+++ Assets/Scripts/TerrainGenerator.cs
@@ -9,7 +9,7 @@ public class TerrainGenerator : MonoBehaviour
 	* Based on the "renderDistance" and player position, generate individual chunks and place them in PCTerrain.
 	*/
 
-	// Private instance of the FastNoise library from Jordan Peck.
+	// Private instance of the FastNoise library by Jordan Peck.
 	private FastNoise noise;
 
 	void Start()
