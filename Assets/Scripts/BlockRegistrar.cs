8a41a3b77806cca76ddd7d405061246b9baf0793
Fehniix <29045904+Fehniix@users.noreply.github.com>
2021-05-23 16:00:08 +0200

    First commit

---
+++ Assets/Scripts/BlockRegistrar.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class BlockRegistrar : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Blocks.RegisterBlock<Cobblestone>("cobblestone");
+		Blocks.RegisterBlock<Bedrock>("bedrock");
+    }
+}
