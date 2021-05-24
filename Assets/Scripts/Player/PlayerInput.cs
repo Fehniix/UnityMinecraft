75d1d62f745fc86422171c0776428a3c35d25c4d
Fehniix <29045904+Fehniix@users.noreply.github.com>
2021-05-24 22:38:16 +0200

    Implementing jump...

--- Assets/Scripts/Player/PlayerInput.cs
+++ Assets/Scripts/Player/PlayerInput.cs
@@ -12,7 +12,17 @@ public class PlayerInput : MonoBehaviour
 	/// <summary>
 	/// The downwards acceleration due to gravity.
 	/// </summary>
-	public float gravitationalAcceleration = -9.8f;
+	private float gravitationalAcceleration = -9.81f;
+
+	/// <summary>
+	/// Current magnitude of the downwards velocity vector.
+	/// </summary>
+	private float downwardsSpeed = -9.81f;
+
+	/// <summary>
+	/// Height of the jump that can be performed by the player.
+	/// </summary>
+	private float jumpHeight = 1f;
 
 	/// <summary>
 	/// Reference to the player's CharacterController. Used to change the player's position.
@@ -31,17 +41,35 @@ public class PlayerInput : MonoBehaviour
 		float deltaX = Input.GetAxis("Horizontal") * speed;
 		float deltaY = Input.GetAxis("Vertical") * speed;
 
-		Vector3 movement = new Vector3(deltaX, this.gravitationalAcceleration, deltaY);
+		// Movement along the x-z axis
+		Vector3 movement_xz = new Vector3(deltaX, 0, deltaY);
+		// Movement along the y axis
+		Vector3 movement_y	= new Vector3(0, this.downwardsSpeed, 0);
 
 		// When moving diagonally, ||i+k|| > speed; "clamping" the movement vector's magnitude solves the issue.
-		movement = Vector3.ClampMagnitude(movement, this.speed);
-		
+		movement_xz = Vector3.ClampMagnitude(movement_xz, this.speed);
+
+		if (this.characterController.isGrounded)
+		{
+			movement_y = Vector3.zero;
+			this.downwardsSpeed = this.gravitationalAcceleration;
+			
+			if (Input.GetKeyDown(KeyCode.Space))
+			{
+				movement_y += new Vector3(0, Mathf.Sqrt(this.jumpHeight * -3.0f * this.gravitationalAcceleration), 0);
+			}
+		}
+		else
+			// Emulate gravitational acceleration.
+			this.downwardsSpeed += this.gravitationalAcceleration * Time.deltaTime;
+
 		// Now imagine the game running at 1200fps without the following line. Also, given the game is paused,
 		// Time.deltaTime equals zero: movement is zeroed out too.
-		movement *= Time.deltaTime;
+		movement_xz *= Time.deltaTime;
+		movement_y 	*= Time.deltaTime;
 
 		// Transform direction to global space coordinates.
-		movement = this.transform.TransformDirection(movement);
+		Vector3 movement = this.transform.TransformDirection(movement_xz + movement_y);
 
         this.characterController.Move(movement);
     }
