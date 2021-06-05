85b13b3d845c559b3ea6e30090f0c0607bdd697e
Fehniix <29045904+Fehniix@users.noreply.github.com>
2021-06-05 15:21:43 +0200

    Started implementing an abstracted inventory architecture.

---
+++ Assets/Scripts/Inventory/InventoryContainer.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InventoryContainer : MonoBehaviour
+{
+	/// <summary>
+	/// The UUID associated with the inventory container.
+	/// </summary>
+	private string id;
+
+	/// <summary>
+	/// Reference to the items grid.
+	/// </summary>
+	private GameObject[] items;
+
+	/// <summary>
+	/// The number of items the inventory will contain.
+	/// </summary>
+	[SerializeField]
+	private int itemsCount;
+
+    void Awake()
+    {
+		// Register inventory in InventoryContainers
+		this.id = System.Guid.NewGuid().ToString();
+		InventoryContainers.containers[this.id] = this;
+
+        // Get the reference to the inventory grid.
+		this.items = new GameObject[this.itemsCount];
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Inventory/InventoryContainers.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+/// <summary>
+/// Contains static references to all inventory containers throughout the game.
+/// </summary>
+public static class InventoryContainers
+{
+	/// <summary>
+	/// The inventory containers. Registered and accessible by UUID.
+	/// </summary>
+    public static Dictionary<string, InventoryContainer> containers = new Dictionary<string, InventoryContainer>();
+}
