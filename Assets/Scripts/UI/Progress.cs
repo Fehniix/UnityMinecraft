fda957c1394c4b55117e075c82a5d333a5d5d001
Fehniix <29045904+Fehniix@users.noreply.github.com>
2021-06-08 20:20:35 +0200

    It was effectively too easy, after all. Gosh.

--- Assets/Scripts/UI/Progress.cs
+++ Assets/Scripts/UI/Progress.cs
@@ -22,35 +22,56 @@ public class Progress : MonoBehaviour
 	/// <summary>
 	/// Transform reference of the hide panel.
 	/// </summary>
-	private RectTransform hidePanelTransform;
+	private RectTransform icon;
+
+	/// <summary>
+	/// Transform reference to this RectTransform object.
+	/// </summary>
+	private RectTransform mask;
 
     // Start is called before the first frame update
     void Start()
     {
-		this.hidePanelTransform = this.transform.GetChild(0).GetComponent<RectTransform>();
+		this.mask = this.GetComponent<RectTransform>();
+		this.icon = this.transform.GetChild(0).GetComponent<RectTransform>();
 
 		this.fullSize = new Vector2(
-			this.hidePanelTransform.rect.width, 
-			this.hidePanelTransform.rect.height
+			this.mask.rect.width, 
+			this.mask.rect.height
 		);
 
 		switch(this.orientation)
 		{
+			/**
+			* Note to future self, for clarity.
+			*
+			* Moving the pivot point of a RectTransform also influences the point from which it gets DRAWN by Unity!
+			* Originally, the pivot point is (0.5, 0.5). I'm leveraging it to make it easier to define transformations
+			* along different axes.
+			* Setting the pivot point to (0.5, 0.0f), or TOP, means the mask itself (parent object) needs to be translated
+			* to the bottom by mask.height / 2 because the pivot point is moved downwards (moving the mask upwards) by half of the mask size.
+			* Same thing applies for all the other axes and directions.
+			*/
+
 			case ProgressOrientation.TOP:
-				this.hidePanelTransform.position += new Vector3(0, this.fullSize.y / 2, 0);
-				this.hidePanelTransform.pivot = new Vector2(0.5f, 1.0f);
+				this.mask.position -= new Vector3(0, this.fullSize.y / 2 + 0.1f, 0);
+				this.mask.pivot = new Vector2(0.5f, 0.0f);
+				this.icon.pivot = new Vector2(0.5f, 0.0f);
 				break;
 			case ProgressOrientation.BOTTOM:
-				this.hidePanelTransform.position -= new Vector3(0, this.fullSize.y / 2, 0);
-				this.hidePanelTransform.pivot = new Vector2(0.5f, 0.0f);
+				this.mask.position += new Vector3(0, this.fullSize.y / 2 + 0.1f, 0);
+				this.mask.pivot = new Vector2(0.5f, 1.0f);
+				this.icon.pivot = new Vector2(0.5f, 1.0f);
 				break;
 			case ProgressOrientation.LEFT:
-				this.hidePanelTransform.position -= new Vector3(this.fullSize.x / 2, 0, 0);
-				this.hidePanelTransform.pivot = new Vector2(0.0f, 0.5f);
+				this.mask.position += new Vector3(this.fullSize.x / 2, 0.1f, 0);
+				this.mask.pivot = new Vector2(1.0f, 0.5f);
+				this.icon.pivot = new Vector2(1.0f, 0.5f);
 				break;
 			case ProgressOrientation.RIGHT:
-				this.hidePanelTransform.position += new Vector3(this.fullSize.x / 2, 0, 0);
-				this.hidePanelTransform.pivot = new Vector2(1.0f, 0.5f);
+				this.mask.position -= new Vector3(this.fullSize.x / 2, 0.1f, 0);
+				this.mask.pivot = new Vector2(0.0f, 0.5f);
+				this.icon.pivot = new Vector2(0.0f, 0.5f);
 				break;
 		}
     }
@@ -61,19 +82,41 @@ public class Progress : MonoBehaviour
     public void UpdateProgress(int progress)
 	{
 		this.progress = progress;
-		this.progress /= 100.0f;
+		this.progress = this.progress / 100.0f;
 
 		if (this.orientation == ProgressOrientation.TOP)
-			this.hidePanelTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, this.fullSize.y - this.fullSize.y * this.progress);
-
+		{
+			/**
+			* Note to future self who's going to forget all these empirical formulas.
+			* "Be as big as progress", quoting. Lol.
+			* The size of the mask directly reflects progress: 10% -> mask is 1/10 its original size.
+			* The position of the icon itself (within the mask) changes by half the progress towards the opposite direction
+			* to reflect the new mask size:
+			* Progress is 25%. Mask size gets reduced to 1/4.
+			* Before changing position, the icon is now positioned at the center of the smaller mask.
+			* Moving the icon down by half the new size of the mask, the icon is placed correctly.
+			*/
+			this.mask.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, this.fullSize.y * this.progress);
+			this.icon.Translate(new Vector3(0, -this.mask.rect.height / 2, 0));
+		}
+			
 		if (this.orientation == ProgressOrientation.BOTTOM)
-			this.hidePanelTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, this.fullSize.y - this.fullSize.y * this.progress);
+		{
+			this.mask.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, this.fullSize.y * this.progress);
+			this.icon.Translate(new Vector3(0, this.mask.rect.height / 2, 0));
+		}
 
 		if (this.orientation == ProgressOrientation.LEFT)
-			this.hidePanelTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, this.fullSize.x - this.fullSize.x * this.progress);
+		{
+			this.mask.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, this.fullSize.x * this.progress);
+			this.icon.Translate(new Vector3(this.mask.rect.width / 2, 0, 0));
+		}
 
 		if (this.orientation == ProgressOrientation.RIGHT)
-			this.hidePanelTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, this.fullSize.x - this.fullSize.x * this.progress);
+		{
+			this.mask.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, this.fullSize.x * this.progress);
+			this.icon.Translate(new Vector3(-this.mask.rect.width / 2, 0, 0));
+		}
 	}
 }
 
