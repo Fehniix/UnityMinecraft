379aba7219abb90278e47be41096ffde8c7252da
Fehniix <29045904+Fehniix@users.noreply.github.com>
2021-06-11 20:44:16 +0200

    Definitely not handing in a cheated inventory.

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -13,25 +13,7 @@ public class Controller : MonoBehaviour
 
 		GameObject.Find("Player").transform.position = new Vector3(8, 40, 8);
 
-		for (int i = 0; i < 9; i++)
-			InventoryContainers.hotbar.items[i] = new InventoryItem("cobblestone");
-
-		for (int i = 0; i < 26; i++)
-			InventoryContainers.inventory.items[i] = new InventoryItem("torch");
-
-		InventoryContainers.hotbar.items[0] = new InventoryItem("furnace");
-		InventoryContainers.hotbar.items[2].quantity = 64;
-		InventoryContainers.hotbar.items[8] = new InventoryItem("craftingTable");
-
 		GUI.hotbar.UpdateGUI();
-
-		string[,] requirements = new string[3,3] {
-				{"torch", "torch", null},
-				{"torch", "torch", null},
-				{null, null, null}
-		};
-		
-		CraftingRecipeRegistry.RegisterRecipe(new CraftingRecipe(requirements, new CraftingResult("cobblestone", 64)));
     }
 
     // Update is called once per frame
